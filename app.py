62f12a1addfd0c64a5d8b5f7fe23b34d67631106
Huai <leonowpower@gmail.com>
2022-01-09 12:38:21 +0800

    app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from linebot.models import (
 app = Flask(__name__)
 import random
 
-line_bot_api = LineBotApi('eS4z9G9LP5MpigySxQnBhnezzk8Fx8SRUY5O+FxCu+vXmSMz14xjDwa3Vpik+LBPpEjK9MyW7vJW3ovYlnZo+thBViN8ZgbYq6EhG4HLcSd/hzsb73UY5eJw32nxSZVBdJYbltsSOlTYWhnimi1sOQdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('9390e26a32374a3789253a5c72c19c65')
+line_bot_api = LineBotApi('BKeap2C2/GF12NIzSB7FmL4pX28xE+l2u0MJhUsON5CR2PM1JutNBtZV0bt7VDkSpEjK9MyW7vJW3ovYlnZo+thBViN8ZgbYq6EhG4HLcSdWWO5f2giuwHWosKD9YIm1yTQM45dLNCa4u04pmiyqygdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('e505df37bcb1b4c392c7bc12396eb941')
 
 
 @app.route("/callback", methods=['POST'])
@@ -39,25 +39,27 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = random.randint(0,20)
+    s = random.randint(0,30)
     if s == 5:
         r = '閉嘴'
     elif '早餐' in msg:
-        r = '吃我屌'
+        r = '幾點哪裡'
     elif '午餐' in msg:
-        r = '吃我屌'
+        r = '吃早餐店不叫午餐好不好'
     elif '晚餐' in msg:
         r = '吃我屌'
     elif '宵夜' in msg:
         r = '吃我屌'
     elif '要幹嘛' in msg:
         r = random.choice([
-            '做愛', '打球', '健身', '去看你女朋友洗澡', '不知道', '上你', '你覺得去上課怎麼樣', '跟妹妹壞壞'])
+            '做愛', '打球', '健身', '去看女生洗澡', '不知道', '上你', '上課怎麼樣', '約臭妹妹壞壞'])
     elif '出門' in msg:
-        r = '誒你要確定誒'
+        r = random.choice(['誒你要確定誒','是不是在騙','最好不要比我晚到'])
     elif '打球' in msg:
-        r = '不去的都是俗仔'
-
+        r = random.choice(['不去的都是俗仔','想被電？菜雞'])
+    elif '有人在臭' in msg:
+        r = '又在臭'
+    
         
 
 
